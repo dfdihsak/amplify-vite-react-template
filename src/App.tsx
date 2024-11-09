ed7c74cab3f0ce749eadc5b3410c138f3cfa4b56
dfdihsak <jessie.sui.js@gmail.com>
2024-11-08 21:17:47 -0500

    added favicon, removed list display

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
-
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: window.prompt("Enter your email:") });
   }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
+      <h1><img></img></h1>
+      <h1>Join our waitlist for a free trial!</h1>
+      <button onClick={createTodo}>Join Waitlist</button>
+      {/* <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
-      </ul>
-      <div>
+      </ul> */}
+      {/* <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
